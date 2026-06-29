798396ea3ebec21d16964483dbfbc8f3decfa828
Ann Song <gmsong1978@gmail.com>
2026-06-28 21:38:01 -0400

    URDF file

---
+++ src/tomato_description/launch/display.launch.py
@@ -0,0 +1,70 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, Command
+
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    package_name = "tomato_description"
+
+    pkg_share = get_package_share_directory(package_name)
+
+    default_model_path = os.path.join(
+        pkg_share,
+        "urdf",
+        "tomato_arm.urdf.xacro",
+    )
+
+    model_arg = DeclareLaunchArgument(
+        "model",
+        default_value=default_model_path,
+        description="Absolute path to robot URDF/Xacro file",
+    )
+
+    robot_description = {
+        "robot_description": Command([
+            "xacro ",
+            LaunchConfiguration("model"),
+        ])
+    }
+
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name="robot_state_publisher",
+        output="screen",
+        parameters=[robot_description],
+    )
+
+    joint_state_publisher_gui_node = Node(
+        package="joint_state_publisher_gui",
+        executable="joint_state_publisher_gui",
+        name="joint_state_publisher_gui",
+        output="screen",
+    )
+
+    rviz_config = os.path.join(
+        pkg_share,
+        "rviz",
+        "tomato_robot.rviz",
+    )
+
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="screen",
+        arguments=["-d", rviz_config],
+    )
+
+    return LaunchDescription([
+        model_arg,
+        robot_state_publisher_node,
+        joint_state_publisher_gui_node,
+        rviz_node,
+    ])
